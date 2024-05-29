76f82f1811ebbbad4b1f6e1acbe7d26b63038394
BishnuKumar Bhakat <bishnukumar_bhakat@epam.com>
2024-05-29 18:57:20 +0530

    tools changes

--- 2-TOOLS/server.js
+++ 2-TOOLS/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+const compression = require('compression');
+app.use(compression());
 app.use(express.static('build'));
 
 const listener = app.listen(8080, function () {
